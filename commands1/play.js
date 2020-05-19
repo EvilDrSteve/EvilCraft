b33437c598cc1a0ac48066ba3831d1667d8b322c
EvilDrSteve <38597526+EvilDrSteve@users.noreply.github.com>
2020-05-20 00:20:07 +0500

    Update play.js

--- commands1/play.js
+++ commands1/play.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, msg, args) => {
 			
 			if(!msg.content.startsWith(config.PREFIX)) return
 			
-			let rawdata = fs.readFileSync('./playdata.json').catch(err => if(err) return console.log(err)); let data = JSON.parse(rawdata); console.log(data);
+			let rawdata = fs.readFileSync('./playdata.json').catch(err => {if(err) return console.log(err)}); let data = JSON.parse(rawdata); console.log(data);
 			
 			
 			if(msg.author.bot) return;
