82645418079c39c40eae197869cbead48079c059
EvilDrSteve <2001.shehryar@gmail.com>
2020-07-08 20:10:30 +0500

    teemoved stuff

--- commands1/play.js
+++ commands1/play.js
@@ -1,20 +1,23 @@
 const config = require("../config.json")
 const fs = require('fs')
+const cooldown = new Set()
 
 module.exports.run = async (bot, msg, args) => {
 			
 			if(!msg.content.startsWith(config.PREFIX)) return
 			
-			let rawdata = fs.readFileSync('./commands1/playdata.json'); let data = JSON.parse(rawdata); console.log(data);
+			let rawdata = fs.readFileSync('./playdata.json'); let data = JSON.parse(rawdata); console.log(data);
 			
-			
+			if(cooldown.has(msg.author.id)) return msg.channel.send("Ah yes, joining a second after leaving, how about no.")
 			if(msg.author.bot) return;
 			if(!msg.member.roles.find(r => r.name === "EvilCraft")) return;
 			
 				if(!data[msg.author.id]){
 				data[msg.author.id] = {
 					ingame: 0,
-					message: 0
+					message: 0,
+					gt: 0,
+					count: 0
 				}
 			}
 			
@@ -25,17 +28,66 @@ module.exports.run = async (bot, msg, args) => {
 			}
 			
 				
-			user = msg.author
+	 let user = msg.author
 			channel = msg.channel.name
 			if(data[user.id].ingame !== 0) return msg.channel.send("You are already in a game!")
-			 bot.guilds.get(config.SERVER_ID).channels.get("711048304502374493").send(`${user} joined the realm`).then(m => {
+			
+	if(data[user.id].gt == 0) {
+		data[user.id].gt = user.username
+		}
+			
+	let count = 0
+	let min = 0
+	data[user.id].count = 0
+	var embed = new Discord.RichEmbed()
+ .setColor(config.RED)
+	.setTitle(`${data[user.id].gt}`)
+//	.setDescription("**Joined the Realm**")
+ .addField('Playing For', "0 Minutes")
+	.setThumbnail(user.avatarURL)
+	.setFooter(`AKA ${user.username}`, user.avatarURL)
+	.setTimestamp()
+	 bot.guilds.get(config.SERVER_ID).channels.get("711048304502374493").send(embed).then(m => {
 			 	data[user.id].ingame = 1
 			 	data[user.id].message = m.id
 			 	data["playing"].now = data["playing"].now + 1
 			 	
-			 					fs.writeFileSync("./commands1/playdata.json", JSON.stringify(data), (err=>{
+			 					fs.writeFileSync("./playdata.json", JSON.stringify(data), (err=>{
 						if(err) return console.log(err)
 					}))
+				
+				
+				const counter = setInterval(() => {//if(data[user.id].ingame == 1) {
+					let rawdata = fs.readFileSync('./playdata.json'); let data = JSON.parse(rawdata);
+					if(data[user.id].ingame == 0) return clearInterval(counter);
+					 console.log(data);
+	 	 console.log(data[user.id].count)
+	 //	 count++
+	 	 data[user.id].count++
+	 	 var embed1 = new Discord.RichEmbed()
+  	 .setColor(config.RED)
+	 	 .setTitle(`${data[user.id].gt}`)
+	 	 .addField('Playing for', `${data[user.id].count} Minutes`)
+	.setThumbnail(user.avatarURL)
+	.setFooter(`AKA ${user.username}`, user.avatarURL)
+	.setTimestamp()
+	
+console.log(data[user.id])
+	bot.guilds.get(config.SERVER_ID).channels.get("711048304502374493").fetchMessage(data[user.id].message).then(e => e.edit(embed1))
+	
+	fs.writeFileSync("./playdata.json", JSON.stringify(data), (err=>{
+						if(err) return console.log(err)
+					}))
+	 	 //}
+	 	/*  else {
+	 	 	 clearInterval(counter)
+	 	 	 min++
+	 	 	 console.log(min)
+	 	 	 count = 0
+	 	 	// m.edit(count)
+	 	 	 
+	 	 	  }*/
+	 	 }, 1000 * 60)
 					bot.channels.get("712130741865283605").setName(`Now Playing: ${data["playing"].now}`)
 					
 if(data["playing"].now <= 1)	{
@@ -45,6 +97,11 @@ bot.user.setActivity(`with ${data["playing"].now} Person`);
 }
 					})
 					console.log(data)
+					
+					cooldown.add(msg.author.id);
+					setTimeout(() => {
+						cooldown.delete(msg.author.id)
+					}, 1000 * 65)
 			 }
 
 module.exports.config = {
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-const config = require("./config.json"); 
+const config = require("./config.json");
 
 //const PREFIX = "!";
 
@@ -11,48 +11,49 @@ bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
 fs.readdir("./commands1", (err, files) => {
-	
-	if(err) console.log(err)
-	
-	let jsfile = files.filter(f => f.split(".").pop() === "js")
-	if(jsfile.length <= 0) {
-		return console.log("Couldnt find commands");
-	}
-	console.log(`${jsfile}`)
-	jsfile.forEach((f, i) => {
-		let pull = require(`./commands1/${f}`);
-		bot.commands.set(pull.config.name, pull);
-		pull.config.aliases.forEach(alias => {			bot.aliases.set(alias, pull.config.name)
-		});
-	})
+
+  if (err) console.log(err)
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if (jsfile.length <= 0) {
+    return console.log("Couldnt find commands");
+  }
+  console.log(`${jsfile}`)
+  jsfile.forEach((f, i) => {
+    let pull = require(`./commands1/${f}`);
+    bot.commands.set(pull.config.name, pull);
+    pull.config.aliases.forEach(alias => {
+      bot.aliases.set(alias, pull.config.name)
+    });
+  })
 })
 
 bot.on('ready', () => {
-	console.log("Now online")
+  console.log("Now online")
 })
 
 bot.on('message', msg => {
-	if(msg.author.bot || msg.channel.type == "dm") return;
-	
-let prefix = config.PREFIX
-let messageArray = msg.content.split(" ")
-let cmd = messageArray[0]
-let args = messageArray.slice(1)
+  if (msg.author.bot || msg.channel.type == "dm") return;
+
+  let prefix = config.PREFIX
+  let messageArray = msg.content.split(" ")
+  let cmd = messageArray[0]
+  let args = messageArray.slice(1)
 
 
-let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-if(commandfile) commandfile.run(bot,msg,args)
+  let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+  if (commandfile) commandfile.run(bot, msg, args)
 })
 
 
 //const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 //for(const file of commandFiles){
-	//const command = require(`./commands/${file}`);
-	
-	//bot.commands.set(command.name, command)
-	//console.log(`${file} loaded`)
+//const command = require(`./commands/${file}`);
+
+//bot.commands.set(command.name, command)
+//console.log(`${file} loaded`)
 //};
 
 //bot.commands.get('start').execute();
 
-bot.login(process.env.token);
+bot.login(process.env.token)
