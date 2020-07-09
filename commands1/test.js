85c1496f8a6496f8338a46d60ef76672402a603f
EvilDrSteve <2001.shehryar@gmail.com>
2020-07-10 00:29:03 +0500

    b

--- commands1/test.js
+++ commands1/test.js
@@ -5,14 +5,14 @@ const fs = require('fs')
 const cooldown = new Set()
 const Data = require('../Models/test.js')
 var count = 0
-var interval = setInterval({
+
+var interval = setInterval(() => {
   count++
   console.log(count)
 }, 1000 * 1)
-}
 module.exports.run = async (bot, msg, args) => {
   //var test = await Data.find().byName(msg.author.tag)
-  interval = setInterval({
+  interval = setInterval(() =>{
     count++
     console.log(count)
   }, 1000 * 100)
