4a281927a6857ca98bf102c0c9e75ffeb07db1d4
EvilDrSteve <2001.shehryar@gmail.com>
2020-07-10 00:26:12 +0500

    h

--- commands1/test.js
+++ commands1/test.js
@@ -4,17 +4,20 @@ const mongoose = require('mongoose')
 const fs = require('fs')
 const cooldown = new Set()
 const Data = require('../Models/test.js')
-
+var count = 0
+var interval = setInterval({
+  count++
+  console.log(count)
+}, 1000 * 1)
+}
 module.exports.run = async (bot, msg, args) => {
   //var test = await Data.find().byName(msg.author.tag)
-   bot.guilds.cache.get(config.SERVER_ID).channels.cache.get("712130741865283605").setName(`Now Playing:69`).catch(err => {
-     console.log(err)
-   })
- // console.log(test)
-  //msg.channel.send(test)
+  interval = setInterval({
+    count++
+    console.log(count)
+  }, 1000 * 100)
+
 }
-
-
   
   	 module.exports.config = {
   	   name: "test",
