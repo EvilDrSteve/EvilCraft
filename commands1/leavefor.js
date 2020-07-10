5905c817335bf4fe954fc3c2719579f058421989
EvilDrSteve <2001.shehryar@gmail.com>
2020-07-10 17:32:45 +0500

    b

---
+++ commands1/leavefor.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const config = require("../config.json")
+const mongoose = require('mongoose')
+const fs = require('fs')
+const cooldown = new Set()
+const Data = require('../Models/joinleavedata.js')
+
+module.exports.run = async (bot, msg, args) => {
+
+  if (!msg.content.startsWith(config.PREFIX)) return
+
+  if (msg.author.bot) return;
+  // if (!msg.member.roles.find(r => r.name === "EvilCraft")) return;
+  if (!msg.mentions.users.first()) return
+  var mention = msg.mentions.users.first()
+  user = await Data.findOne().byID(mention.id)
+  channel = msg.channel.name
+
+  if (user.ingame !== 1) return msg.channel.send("Cant leave a game if havent joined one!")
+
+  bot.guilds.cache.get(config.SERVER_ID).channels.cache.get("711048304502374493").messages.fetch(user.message).then(m => m.delete()).catch(err => {
+    console.log(err)
+  })
+
+  user.ingame = 0
+  user.message = 0
+  user.count = 0
+
+  await user.save()
+
+  online = await Data.find().byIngame()
+  bot.user.setActivity(`with ${online.length} others`, { type: "PLAYING" });
+}
+
+
+module.exports.config = {
+  name: "leavefor",
+  aliases: ["lf"]
+}
