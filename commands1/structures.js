a4cbadc20fc173f691652317b728d9b162f0b9b7
EvilDrSteve <2001.shehryar@gmail.com>
2020-07-14 00:00:46 +0500

    b

--- commands1/structures.js
+++ commands1/structures.js
@@ -74,7 +74,7 @@ module.exports.run = async (bot, msg, args) => {
 
     for (let i = 0; i < structures.length; i++) {
       console.log(structures[i])
-      embed.addField(structures[i].Type, `x: ${structures[i].x}, z: ${structures[i].z}`)
+      embed.addField(structures[i].Type, `x: ${structures[i].Coords.x}, z: ${structures[i].Coords.z}`)
     }
 
     msg.channel.send(embed)
